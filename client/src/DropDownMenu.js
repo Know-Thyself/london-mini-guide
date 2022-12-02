d5b12c0e4bb2aea2d92632453df619d497f01087
Biruk Kebede <biruk.key@gmail.com>
2022-12-02 00:57:20 +0000

    customized mui select component

--- client/src/DropDownMenu.js
+++ client/src/DropDownMenu.js
@@ -5,27 +5,78 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
-
-export default function DropDownMenu({ selectedCity, setSelectedCity, setPage }) {
-  const cities = ['Harrow', 'Heathrow', 'Stratford']
+export default function DropDownMenu({
+	selectedCity,
+	setSelectedCity,
+	setPage,
+}) {
+	const cities = ['Harrow', 'Heathrow', 'Stratford'];
+	const [select, setSelect] = React.useState('Select City')
 
 	const handleChange = (e) => {
-    console.log(e.target.value)
+		setSelect('');
 		setSelectedCity(e.target.value);
 		setPage(0);
 	};
 
 	return (
-		<Box sx={{ minWidth: 140 }}>
-			<FormControl fullWidth size='small'>
-				<InputLabel id='select-label'>Select A City</InputLabel>
+		<Box
+			sx={{
+				minWidth: 140,
+				'& .MuiSvgIcon-root': {
+					color: 'white',
+				},
+			}}
+		>
+			<FormControl
+				sx={{ color: 'white', backgroundColor: '#8d99ae' }}
+				fullWidth
+				size='small'
+				focused={false}
+			>
+				<InputLabel
+					sx={{ color: 'white' }}
+					id='select-label'
+					disableAnimation={true}
+					shrink={false}
+				>
+					{select}
+				</InputLabel>
 				<Select
 					labelId='select-label'
 					id='select'
 					value={selectedCity}
 					label='Select A City'
 					onChange={handleChange}
-					style={{ backgroundColor: 'aliceblue' }}
+					sx={{
+						backgroundColor: '#2b2d42',
+						color: 'white',
+						'& .MuiMenuItem-gutters': {
+							backgroundColor: 'white',
+							color: 'white',
+						},
+					}}
+					MenuProps={{
+						PaperProps: {
+							sx: {
+								'& .MuiMenuItem-root.Mui-selected': {
+									color: 'black',
+									backgroundColor: '#edf2f4',
+								},
+								'& .MuiMenuItem-root.Mui-selected:hover': {
+									backgroundColor: '#8d99ae',
+								},
+								'& .MuiMenuItem-root:hover': {
+									backgroundColor: '#2b2d42',
+									color: 'white',
+								},
+								'& .MuiList-root': {
+									backgroundColor: '#edf2f4',
+									color: 'black',
+								},
+							},
+						},
+					}}
 				>
 					{cities.map((city, index) => (
 						<MenuItem key={index} value={city}>
--- client/src/Header.js
+++ client/src/Header.js
@@ -2,7 +2,7 @@ const Header = () => {
   return (
 		<header className='header'>
 			<h2>London Sub City Mini Guide </h2>
-			<h4> Select a city and category </h4>
+			<h4 className='sub-text'> Select a city and category </h4>
 		</header>
 	);
 }
